3765c12c39a8d92b36b3109df90b64d203e2024c
Jared Ridabock <jaredridabock@gmail.com>
2021-03-26 15:26:58 -0700

    further mods to madlib

--- app.js
+++ app.js
@@ -1,5 +1,45 @@
 // import functions and grab DOM elements
 
+// DOM Elements for input objects
+const verbIn1 = document.getElementById('verb-1');
+const verbIn2 = document.getElementById('verb-2');
+const verbIn3 = document.getElementById('verb-3');
+const verbIn4 = document.getElementById('verb-4');
+const verbIn5 = document.getElementById('verb-5');
+const verbIn6 = document.getElementById('verb-6');
+const verbIn7 = document.getElementById('verb-7');
+const verbIn8 = document.getElementById('verb-8');
+const verbIn9 = document.getElementsByClassName('verb-9');
+
+const advIn1 = document.getElementById('adverb-1'); 
+
+const adjIn1 = document.getElementById('adjective-1');
+const adjIn2 = document.getElementById('adjective-2');
+
+const nounIn1 = document.getElementById('plural-noun-1');
+
+const button = document.getElementById('button');
+
+// DOM Elements for Span IDs
+
+const verbOut1 = document.getElementById('verb-1-id');
+const verbOut2 = document.getElementById('verb-2-id');
+const verbOut3 = document.getElementById('verb-3-id');
+const verbOut4 = document.getElementById('verb-4-id');
+const verbOut5 = document.getElementById('verb-5-id');
+const verbOut6 = document.getElementById('verb-6-id');
+const verbOut7 = document.getElementById('verb-7-id');
+const verbOut8 = document.getElementById('verb-8-id');
+const verbOut9 = document.getElementsByClassName('verb-miss');
+
+const advOut1 = document.getElementById('adverb-1-id'); 
+
+const adjOut1 = document.getElementById('adjective-1-id');
+const adjOut2 = document.getElementById('adjective-2-id');
+
+const nounOut1 = document.getElementById('plural-noun-1-id');
+
 // initialize state
+
 
 // set event listeners to update state and DOM
